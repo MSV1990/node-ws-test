0023f6c9523a71bbe5c3a780d6a6ca12ebd38cca
msv1990 <htconemailbox@gmail.com>
2019-08-19 00:16:02 +0300

    refactor: console

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-const WS = require('ws')
-const port = process.env.PORT || 9000
-const target = 'ws://st-chat.shas.tel'
-const wss = new WS.Server({ port })
-
-wss.on('connection', function connection(inbound) {
-
-  const outbound = new WS(target)
-  console.log(outbound, inbound)
-});
+console.log(Date.now());
