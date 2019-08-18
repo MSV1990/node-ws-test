8f93a78bc4936c9202aefeae9c089832cacae930
msv1990 <htconemailbox@gmail.com>
2019-08-18 23:52:34 +0300

    refactor: refactor index.js

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
-var WebSocketServer = require("ws").Server
-var http = require("http")
-var express = require("express")
-var app = express()
-var port = process.env.PORT || 5000
-
-app.use(express.static(__dirname + "/"))
-
-var server = http.createServer(app)
-server.listen(port)
-
-console.log("http server listening on %d", port)
-
-var wss = new WebSocketServer({server: server})
-console.log("websocket server created")
+const WS = require('ws')
+const port = process.env.PORT || 9000
+const target = 'ws://st-chat.shas.tel'
+const wss = new WS.Server({ port })
 
 wss.on("connection", function(ws) {
   var id = setInterval(function() {
