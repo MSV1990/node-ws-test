d748e4fb9d529c6bbe7e618578a60665fc280661
msv1990 <htconemailbox@gmail.com>
2019-08-19 00:01:16 +0300

    feat: cleanup

--- index.js
+++ index.js
@@ -3,15 +3,4 @@ const port = process.env.PORT || 9000
 const target = 'ws://st-chat.shas.tel'
 const wss = new WS.Server({ port })
 
-wss.on("connection", function(ws) {
-  var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
-  }, 1000)
-
-  console.log("websocket connection open")
-
-  ws.on("close", function() {
-    console.log("websocket connection close")
-    clearInterval(id)
-  })
-})
+console.log("I'm working")
